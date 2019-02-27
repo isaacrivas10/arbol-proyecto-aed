bdd310237ea1ac092bf4d22c70be47d7d2c05b4a
drunkenboy <idrc74@hotmail.com>
2019-02-26 23:10:41 -0600

    correccion segundo commit

--- pruebas/temp.py
+++ pruebas/temp.py
@@ -1,56 +1,72 @@
 
-class Nodes:
 
-	def __init__(self, value):	
-		self.value= value
+class ListaEnlazada:
+
+	def __init__(self, father):
+		self.father= father # Con father hago referencia un ente superior a la lista enlazada
+		self.first= None
+	
+	def add(self, addV):
+		c = self.first
+		if c is None:
+			self.first = addV
+		else:
+			exist= True
+			while exist: # Mientras exista el nodo
+				if c.getNext() is None: # Si despues de c no hay un nodo
+					c.setNext(addV) # Asignamos como siguiente
+					exist= False
+				else: # Si no
+					c = c.getNext() # Pasamos al siguiente de c
+			
+	def getFather(self):
+		return sefl.first
+
+
+class Nodes(object):
+
+	def __init__(self):	
+		self.value= None
 		self.iAm= None
 		self.branches= None
+		#self.next= None
 	
 	def getType(self):
 		return self.iAm
-
-class ListasEnlazada:
-
-	def __init__(self):
-		self.first= None
-		self.next= None
-	
-	def add(self, addV):
-		if self.first is None:
-			self.first= addv
 
 	def getValue(self):
 		return self.value
 
 	def setNext(self, nextNode):
-		self.next= nextValue
+		self.next= nextNode
 
 	def getNext(self):
 		return self.next
 
-class Archivo(Nodes):
-
-	def __init__(self, value, father=None):
-		self.father= father
-		self.iAm = "archivo"
-		self.value = value
 
 class Carpeta(Nodes):
 
-	def __init__(self, value, father=None):
+	def __init__(self, value):
 		self.iAm = "carpeta"
 		self.value = value
-		self.branches = ListasEnlazada()
-
+		self.branches = ListaEnlazada(self)
+		self.next= None
+		
 	def add(self, addValue):
-		self.branches.append(addValue)
-
-class Root(Nodes):
+		self.branches.add(addValue)
+	
+	
+class Archivo(Nodes):
 
 	def __init__(self, value):
-		self.iAm= "root"
-		self.value= value
-		self.branches = list()
+		self.iAm = "archivo"
+		self.value = value
 
-	def add(self, addValue):
-		self.branches.append(addValue)
+c = Carpeta("carpeta1")
+c.add(Carpeta("carpeta2"))
+c.add(Carpeta("carpeta3"))
+c.add(Carpeta("carpeta4"))
+#c.branches.first.setNext(Carpeta("car"))
+#c.branches.first.getNext().setNext(Carpeta("hola"))
+c.branches.first.add(Carpeta("h"))
+print c.branches.first.branches.first.getValue()
