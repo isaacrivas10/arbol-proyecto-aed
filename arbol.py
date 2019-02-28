672b8c1613a7c202510201903ea662bbcbf02753
drunkenboy <idrc74@hotmail.com>
2019-02-27 23:20:57 -0600

    Correcion 3 commit

--- arbol.py
+++ arbol.py
@@ -1,86 +1,134 @@
 # -*- coding: utf-* -*-
 
+# Esta es una version en limpio de lo que es el Arbol
+# Su version de modificacion esta en pruebas
+
+"""
+	# Modificaciones en base al archivo base Nodes
+	Modificaciones:
+		27/02/18
+		Re-estructuracion del metodo add, moveTo, getBranches,
+		seachInNode
+		Nuevo metodo checkPath
+		Metodo remove sin terminar
+
+
+"""
+
 from nodes import *
+
 
 class Arbol:
 
 	def __init__(self):
 		# El constructor define la raiz de el directorio
-		self.root = Root("root")
+		self.root = Carpeta("root")
 		self.currentNode= self.root
 
 	def getRoot(self):
 		return self.root.getValue()
 
-	def add(self, addValue):
-		addValue.father= self.currentNode
-		self.currentNode.add(addValue)
+	def add(self, addValue, _type):
+		"""
+			Objetivo: Agregar objetos
+
+			Uso:
+				add("nombre_del_archivo", *param)
+				*param puede ser -c de carpeta o -a de archivo
+		"""
+		new= None
+		if _type == "-c":
+			new= Carpeta(addValue)
+		else:
+			new= Archivo(addValue)
+		self.currentNode.add(new)
+
+	def remove(self, remValue):
+		b= self.searchInNode(self.root, remValue)
+		if b != False:
+			print b.getValue()
+		else:
+			print False
+
+	def moveTo(self, moveV, path):
+		"""
+			Tipos de movimiento:
+				- Movimiento en si mismo: Nos direccionamos del nodo actual
+					hacia abajo 
+				- Moviemiento absoluto: Desplazamiento desde la raiz 
+
+			Retorna True si el movimiento se realizo, de lo contrario False
+		"""
+		checked= self.checkPath(path)
+		print checked
+		if checked[0] == "relative":
+			if checked[1] == self.currentNode.getValue():				
+				m_dir= self.searchInNode(self.currentNode, moveV)
+				if m_dir != False:
+					if m_dir.iAm == "carpeta":
+						self.currentNode= m_dir
+						return True
+					else:
+						return False
+		elif checked[0] == "absolute":
+			# Buscamos el valor que esta dentro de root
+			if len(checked) > 2: # Una o mas carpetas despues de root
+				temp = self.searchInNode(self.root, checked[2])				
+				for i in range(3, len(checked)):
+					if temp != False:
+						temp= self.searchInNode(temp, checked[i])
+				if  temp != False:
+					if temp.iAm == "carpeta":
+						self.currentNode = temp
+						return True
+					else:
+						return False
+				else:
+					return False
+			else: 
+				self.currentNode= self.root
+			
+
+	def checkPath(self, path):
+		# Uso: /path/to/move <-- respetar / al incio
+		try:
+			pathCheck= path.split("/")
+			if pathCheck[1] == "root":
+				return ["absolute"] + pathCheck[1:]
+			else:
+				return ["relative"] + pathCheck[1:]
+		except:
+			return ["error", 1] # 1 = error
 
 	def getBranches(self):
-		# Retorna todas las ramas que siguen la raiz
+		# Retorna todas las ramas relativas al nodo actual
 		b= self.currentNode.branches
-		if len(b) > 0:
-			for branch in b:
-				print "/%s/%s" % (self.currentNode.getValue(),branch.getValue())
+		b_len= b.len()
+		b_node= b.first
+		if b_len> 0:
+			for i in range(b_len):
+				print "/%s/%s" % (self.currentNode.getValue(),b_node.getValue())
+				b_node= b_node.getNext()
 		else:
 			print "/%s/" % (self.currentNode.getValue())
 
-	def searchInNode(self, current, carpeta):
-
-		if len(current.branches) > 0: # Si tiene hijos
-			for node in current.branches: # Para cada nodo en sus ramas
-				if node.iAm == "carpeta": # Verifico si es una carpeta
-					if node.getValue() == carpeta: # Si su nombre es igual a la carpeta que busco
-						return node # Retorno el nodo encontrado
-				# De primeras si no es una carpeta se descarta
+	def searchInNode(self, current, value):
+		# Solo busca en un nodo especifico
+		if current.branches.len() > 0: # Si tiene hijos
+			node = current.branches.first
+			for i in range(current.branches.len()): # Para cada nodo en sus ramas
+				if node.getValue() == value: # Si su nombre es igual a lo que busco
+					return node # Retorno el nodo encontrado
+				else:
+					node= node.getNext()
 			return False # Si tiene hijos pero su carpeta no se encontro
 		else:
 			return False # Si no tiene hijos, no hay donde buscar
 
-	def moveTo(self, *params):
-		"""
-			Solamente puede haber movimiento entre nodos de arriba a abajo
-		"""
-		current = self.currentNode
-		executedParams= list(params)
-		if len(executedParams) == 0:
-			print "Sin parametros"
-			pass
-		else:
-			if executedParams[0] != "..":
-				r= self.searchInNode(current, executedParams[0])
-				if r is False:
-					print "La carpeta no existe en este directorio"
-				else:
-					self.currentNode= r
-			else:
-				if current.getValue() == "root":
-					print "raiz"
-				else:
-					self.currentNode= current.father
+	def searchInPath(self, path, value):
 
+		self.checkPath(path)
+
+	
 	def getCurrentNode(self):
 		return self.currentNode
-
-# Crea una instancia del arbol
-arbol = Arbol()
-
-# Crea un archivo y una carpeta
-ar= Archivo("hola")
-#ar2= Archivo("hola")
-ca= Carpeta("pruebaCarpeta")
-ca2= Carpeta("esta es una carpeta")
-
-#Retorna el contenido en la raiz
-#arbol.getBranches()
-
-
-
-arbol.add(ca) # Agrego carpeta al arbol
-arbol.add(ar) # Agrego archivo al arbol
-arbol.moveTo("pruebaCarpeta") # Me muevo a esta carpeta
-arbol.add(ca2) # Agrego carpeta al arbol
-arbol.moveTo("esta es una carpeta") # Me muevo a esta carpeta
-arbol.moveTo("..") # Me muevo una posicion arriba del arbol
-arbol.getBranches() # Imprimo las cosas dentro del directorio actual
-print arbol.getCurrentNode().value # Retorno el nombre de la carpeta
--- nodes.py
+++ nodes.py
@@ -1,48 +1,70 @@
+# ESTO FUNCIONA NO MODIFICAR
 
-class Nodes:
+"""
+CAMBIOS:
+	27/02/19	
+	Se deprecia father al no necesitar conocer quien esta arriba
+	Se deprecia la clase Root al ser un derivado de nodo y una carpeta a la vez
+	
 
-	def __init__(self, value):	
-		self.value= value
+"""
+
+class ListaEnlazada:
+
+	def __init__(self):
+		self.first= None
+	
+	def add(self, addV):
+		c = self.first
+		if c is None:
+			self.first = addV
+		else:
+			exist= True
+			while exist: # Mientras exista el nodo
+				if c.getNext() is None: # Si despues de c no hay un nodo
+					c.setNext(addV) # Asignamos como siguiente
+					exist= False
+				else: # Si no
+					c = c.getNext() # Pasamos al siguiente de c
+	
+	def remove(self, remV): # remV hace referencia al VALOR de el objeto, NO es un nodo
+		pass
+
+
+class Nodes(object):
+
+	def __init__(self):	
+		self.value= None
 		self.iAm= None
 		self.branches= None
-		self.father= None
+			
+	def getType(self):
+		return self.iAm
 
 	def getValue(self):
 		return self.value
 
-	def setNext(self, nextValue):
-		self.next= nextValue
+	def setNext(self, nextNode):
+		self.next= nextNode
 
 	def getNext(self):
 		return self.next
 
-	def getType(self):
-		return self.iAm
-
-
-class Archivo(Nodes):
-
-	def __init__(self, value, father=None):
-		self.father= father
-		self.iAm = "archivo"
-		self.value = value
 
 class Carpeta(Nodes):
 
-	def __init__(self, value, father=None):
+	def __init__(self, value):
 		self.iAm = "carpeta"
 		self.value = value
-		self.branches = list()
-
+		self.branches = ListaEnlazada()
+		self.next= None
+		
 	def add(self, addValue):
-		self.branches.append(addValue)
-
-class Root(Nodes):
+		self.branches.add(addValue)
+	
+	
+class Archivo(Nodes):
 
 	def __init__(self, value):
-		self.iAm= "root"
-		self.value= value
-		self.branches = list()
-
-	def add(self, addValue):
-		self.branches.append(addValue)
+		self.iAm = "archivo"
+		self.value = value
