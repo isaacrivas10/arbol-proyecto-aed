5cb8170668068d8837a16898b7d775ed79a9f7f7
drunkenboy <idrc74@hotmail.com>
2019-03-04 19:28:34 -0600

    Ya perdi la cuenta de los commit xd

--- pruebas/a.py
+++ pruebas/a.py
@@ -17,17 +17,20 @@ arbol.add("carpeta2", "-c") # mkdir carpeta2
 
 arbol.moveNodeTo("/archivo", "/node") # mv /carpeta /node
 # Si te das cuenta movimos carpeta que se ubica en / estando en /node
+# Osea que moveNodeTo mueve el path en si no si es archivo,
+# si hay problema hay que cambiarlo
+
 arbol.add("carpeta3", "-c")
 
-#arbol.moveToPosition("/", "/")
+#arbol.moveToPosition("/", "/") # por si hace cd /
 
-arbol.moveToPosition("carpeta3", "node")
+arbol.moveToPosition("carpeta3", "node") # cd carpeta3 estando en /node
 
 print arbol.currentPath
 
 arbol.add("hola", "-c")
 
-arbol.moveToPosition("hola", "carpeta3")
+arbol.moveToPosition("hola", "carpeta3") # cd hola estando en carpeta3
 
 arbol.add("archivoHEHE", "-a")
 
@@ -35,6 +38,6 @@ arbol.moveToPosition("/", "/")
 
 print arbol.currentPath
 
-arbol.moveToPosition("hola", "/node/carpeta3")
+arbol.moveToPosition("hola", "/node/carpeta3") # cd /node/carpeta3/hola
 
 print arbol.currentPath
